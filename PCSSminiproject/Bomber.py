b1f2c00697cef7150f2c81ae0dc01c65826507fe
volous <tlindh19@student.aau.dk>
2020-10-10 13:18:04 +0200

    removed threading and added comments

--- PCSSminiproject/Bomber.py
+++ PCSSminiproject/Bomber.py
@@ -1,6 +1,8 @@
 import time
 import _thread
 import pygame
+
+
 class Bomb:
 
     # bRad is blastRadius
@@ -11,9 +13,12 @@ class Bomb:
         self.bSecs = bSecs
         self.bReload = bReload
         self.timer_start = False
-        self.bomb_state = ['1', '2', '3']
+        self.bomb_state_one = pygame.image.load("Res/bomb1.png")
+        self.bomb_state_two = pygame.image.load("Res/bomb2.png")
+        self.bomb_state_three = pygame.image.load("Res/bomb3.png")
 
-    def bomb(self):
+
+    def bomb(self, screen):
         # sets secs to be equal to bSecs
         secs = self.bSecs
         # same timer as in timer method
@@ -21,13 +26,12 @@ class Bomb:
             secs -= 1
             time.sleep(1)
             # if statements that check for the timer position and prints the corespondent state
-            if secs == 4:
-                print(self.bomb_state[0])
-            if secs == 3:
-                print(self.bomb_state[1])
-            if secs == 2:
-                print(self.bomb_state[2])
-
+            if secs == 5 and self.timer_start:
+                screen.blit(self.bomb_state_one, (round(10), round(10)))
+            if secs == 3 and self.timer_start:
+                screen.blit(self.bomb_state_two, (round(10), round(10)))
+            if secs == 1 and self.timer_start:
+                screen.blit(self.bomb_state_three, (round(10), round(10)))
     # timer method
     def timer(self):
         # sets secs to be equal to bSecs
@@ -38,7 +42,7 @@ class Bomb:
             # sleeps for 1 second and stops the while loop continuing for 1 second
             time.sleep(1)
             # of secs is = to 0 code is executed
-            if secs == 0:
+            if secs == 1:
                 print("boom")
                 # resets secs to start point
                 secs = self.bSecs
